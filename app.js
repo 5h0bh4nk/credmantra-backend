2375601f48913a33f045639a5364de79d980db31
Shubhank <kul.shubhank207@gmail.com>
2023-01-24 19:05:32 +0530

    rate limiter added

--- app.js
+++ app.js
@@ -5,6 +5,14 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var mongoose = require("mongoose");
 var cors = require("cors");
+const rateLimit = require('express-rate-limit')
+
+const apiLimiter = rateLimit({
+	windowMs: 15 * 60 * 1000, // 15 minutes
+	max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+	standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+	legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+});
 
 require("dotenv").config();
 
@@ -29,6 +37,7 @@ var corsOptionsDelegate = function (req, callback) {
 }
 
 app.use(cors(corsOptionsDelegate));
+app.use(apiLimiter);
 
 // view engine setup
 app.use(logger('dev'));
