ff7d10124fcb2dc533c7ce2609fc4f93f236ce91
shubhank <kul.shubhank207@gmail.com>
2023-03-24 23:27:47 +0530

    commented

--- app.js
+++ app.js
@@ -50,10 +50,10 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
 app.use('/api' + API_VERSION + '/', indexRouter);
-app.use('/api' + API_VERSION + '/users', usersRouter);
-app.use('/api' + API_VERSION + '/auth', authRouter);
-app.use('/api' + API_VERSION + '/auth/google', googleAuthRouter);
-app.use('/api' + API_VERSION + '/partner', partnerRouter);
+// app.use('/api' + API_VERSION + '/users', usersRouter);
+// app.use('/api' + API_VERSION + '/auth', authRouter);
+// app.use('/api' + API_VERSION + '/auth/google', googleAuthRouter);
+// app.use('/api' + API_VERSION + '/partner', partnerRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
