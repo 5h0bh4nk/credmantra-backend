532d77698966b4da65b5ba6836bebfa2eff88340
shubhank <kul.shubhank207@gmail.com>
2023-03-25 15:57:01 +0530

    cors

---
+++ models/query.model.js
@@ -0,0 +1,22 @@
+const { model, Schema } = require("mongoose");
+
+const QuerySchema = new Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    phone: {
+        type: String,
+        required: true
+    },
+    message: {
+        type: String,
+    }
+});
+
+
+module.exports = model("Query", QuerySchema);
--- routes/customer.js
+++ routes/customer.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const Customer = require("../models/customer.model");
+const Query = require("../models/query.model");
 
 // create appication
 router.post("/create", async (req, res, next) => {
@@ -16,6 +17,20 @@ router.post("/create", async (req, res, next) => {
         next({status: 404, message: err.message});
     }
 });
+
+router.post("/query", async (req, res, next) => {
+    try{
+        const application = await Query.create(req.body);
+        res.status(200).json({
+            status: 200,
+            application: application,
+        });
+    }
+    catch(err){
+        console.log(err);
+        next({status: 404, message: err.message});
+    } 
+})
 
 
 module.exports = router;
